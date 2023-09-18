f98a3daa55a93550f74792fb03d6f8700f216b01
shprince1 <princehsylvanus@gmail.com>
2023-09-18 16:15:12 +0100

    animate

--- src/components/project-Card.jsx
+++ src/components/project-Card.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import Bwimage from '../assets/bwimage.png'
-import style from '../styles/projectcard.module.css'
+import React from "react";
+import Bwimage from "../assets/bwimage.png";
+import style from "../styles/projectcard.module.css";
 
 const ProjectCard = (props) => {
   return (
     <div className={style.cardParentDiv}>
-        {/* <h4>{props.title}</h4> */}
-        <h4>{props.title}</h4>
-<img src={props.image}  alt='Bw image' className={style.pImage}/>
-<h4>{props.description}</h4>
+      {/* <h4>{props.title}</h4> */}
+      <h4>{props.title}</h4>
+      <img src={props.image} alt="Bw image" className={style.pImage} />
+      <div className={style.descriptionDiv}>
+        <h4>{props.description}</h4>
+       <p>{props.details}</p><br></br>
+       
+       <a href={props.url}>Visit Link</a>
         
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,76 +1,85 @@
 import React from "react";
 import ProjectCard from "./project-Card";
-import style from '../styles/project.module.css'
-import bwimage from '../assets/bwimage.png';
+import style from "../styles/project.module.css";
+import bwimage from "../assets/bwimage.png";
 // import '../assets/bwimage.png'
-import store from  '../assets/store.png'
-import products from '../assets/products.png'
-import calcu from '../assets/calcu.png'
-import ebm from '../assets/ebm.png'
-import country from '../assets/country.png'
+import store from "../assets/store.png";
+import products from "../assets/products.png";
+import calcu from "../assets/calcu.png";
+import ebm from "../assets/ebm.png";
+import country from "../assets/country.png";
 
 const Project = () => {
-  const data =[{
-    title:'Bankers Ware house',
-    description: 'Design and Development',
-    image: bwimage,
-  },
-  {
-    title:'Bankers Ware house',
-    description: 'Design and Development',
-    image:store,
-  },
-  {
-    title:'Bankers Ware house',
-    description: 'Design and Development',
-    image: products,
-  },
-  {
-    title:'Bankers Ware house',
-    description: 'Design and Development',
-    image: calcu,
-  },
-  {
-    title:'Bankers Ware house',
-    description: 'Design and Development',
-    image: country,
-  },
-  {
-    title:'Bankers Ware house',
-    description: 'Design and Development',
-    image: ebm,
-  },
-  
-
-  
-
-  
-
-
-];
+  const data = [
+    {
+      title: "Bankers Ware house",
+     description: "Business Start-up",
+      details:'Design and Development',
+      image: bwimage,
+      url: 'www.fb.com',
+    },
+    {
+      title: "Bankers Ware house",
+     description: "Business Start-up",
+      details:'Design and Development',
+      image: store,
+    },
+    {
+      title: "Bankers Ware house",
+     description: "Business Start-up",
+      details:'Design and Development',
+      image: products,
+    },
+    {
+      title: "Bankers Ware house",
+     description: "Business Start-up",
+      details:'Design and Development',
+      image: calcu,
+    },
+    {
+      title: "Bankers Ware house",
+     description: "Business Start-up",
+      details:'Design and Development',
+      image: country,
+    },
+    {
+      title: "Bankers Ware house",
+      description: "Business Start-up",
+      details:'Design and Development',
+      image: ebm,
+    },
+  ];
   return (
     <div>
-      <h1>PROJECTS</h1>
+      {/* <h1>PROJECTS</h1> */}
+       
       <div className={style.cardDiv} id="project">
 
-        <div className={style.projectsCardDiv}>{
-          
-          data.map((data, index )=>{
-            return(
-           <ProjectCard 
-           key={index}
-          //  title={data.title}
-           image={data.image}
-          //  description={data.description}
-           />
-            )
-          })
-          }
+      <div className={style.cardText}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis culpa
+          sapiente ullam eum eos dolorem excepturi in? Facere hic sint delectus
+          veritatis, cumque libero iste illum fugiat laudantium aut ea? Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          accusantium et rerum saepe consectetur tempore possimus quasi aut
+          dolores eligendi.
         </div>
-     
-
+       
+       
+        <div className={style.projectsCardDiv}>
+          {data.map((data, index) => {
+            return (
+              <ProjectCard
+                key={index}
+                //  title={data.title}
+                image={data.image}
+                 description={data.description}
+                 details={data.details}
+                 url={data.url}
+              />
+            );
+          })}
+        </div>
       </div>
-    
     </div>
   );
 };
