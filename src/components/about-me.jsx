8873a4d96f307a3920645e39c286a0361564dd23
shprince1 <princehsylvanus@gmail.com>
2023-10-24 09:44:01 +0100

    ne23

--- src/components/about-me.jsx
+++ src/components/about-me.jsx
@@ -2,39 +2,36 @@ import React from 'react'
 import Style from "../styles/aboutme.module.css"
 import styled from 'styled-components'
 import Sliderskill from "../components/sliderskills";
+import Prince from "../assets/prince5.png"
 
 const AboutMe = () => {
   return (
     <>
     
    
-    <div className={Style.parentdiv}>  
-    <div className={Style.h1div}><br /><br />
-    <h1>ABOUT ME</h1>
-
+   <div className={Style.container}>
+    <div className={Style.imageDiv}>
+      <img src={Prince} />
     </div>
+    <div className={Style.text}>
+      <h3>About me</h3>
+      <h2>Consistency is the rule of the game</h2>
+      <div>
+
+      <p>I am SHPrince,</p>
+      <p>A Tech Ethiusiast ,Web developer, Software engineer and a student of computer science. 
+      I am really passionate about web development and software engineering. I love to solve problems and take on challenges especially when it comes to building projects.
+
+      </p>
+      <p>I look forward to colaborating with individuals and corporate bodies in bringing solution, values, and improving what has already been done.
+
+</p>
+      </div>
         
-       
-
-        <div className={Style.paragraphDiv}>
-            <p>I am SHPrince,</p>
-            <p>A Tech Ethiusiast ,Web developer, Software engineer and a student of computer science.</p>
-            <p>I am really passionate about web development and software engineering. I love to solve problems and take on challenges especially when it comes to building projects.</p>
-            <p>I believe in the principle of geting it done and keep making it better. Getting the job done brings me joy, it's a pleasure to keep improving on it.</p>
-            <p>I look forward to colaborating with individuals and corporate bodies in bringing solution, values, and improving what has already been done.</p>
-       
-            <div className={Style.skilSlideDiv}>
-            
-            <div className={Style.slider}>
-                <Sliderskill />
-                
-            </div>
-        </div>
-       </div>
-
       
-            
-            </div>
+    </div>
+
+   </div>
     
     </>
   )
